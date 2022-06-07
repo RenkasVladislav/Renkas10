ed87c477c224b58864f69e8b5d396c38973d09aa
RenkasVladislav <Enofin7@yandex.ru>
2022-06-07 10:45:38 +0300

    test

--- RenkasVV/mysite/blog/urls.py
+++ RenkasVV/mysite/blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 app_name = 'blog'
 urlpatterns = [
  # post views
  path('', views.post_list, name='post_list'),
  path('<int:id>/', views.post_detail, name='post_detail'),
+ path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
--- RenkasVV/mysite/blog/views.py
+++ RenkasVV/mysite/blog/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
 def post_list(request):
     posts = Post.published.all()
     return render(request,
         'blog/post/list.html',
         {'posts': posts})
 def post_detail(request, id):
-    post = get_object_or_404(Post,
-                id=id,
-                status=Post.Status.PUBLISHED)
-    return render(request,
-        'blog/post/detail.html',
-        {'post': post})
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments,
+'form': form})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form,
+'comment': comment})
+
                
